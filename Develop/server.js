e222d803d1611c364f4e7d17856a0c26376ec324
Brittany Morris <brittanymorrisdesign@gmail.com>
2020-02-11 15:21:28 -0500

    Updated html routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,9 +1,20 @@
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
+
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
-// Sets up Express App
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+// Tells node that we are creating an "express" server
 const app = express();
+// Sets an initial port. We"ll use this later in our listener
 const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app for data parsing
@@ -11,6 +22,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-// Importing get, post, and delete functions
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
 require('./routes/apiroutes')(app);
 require('./routes/htmlroutes')(app);
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
+app.listen(PORT, function() {
+  console.log(`App listening on PORT: ${PORT}`);
+});
